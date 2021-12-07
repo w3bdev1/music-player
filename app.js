7649f0d6e5204055333447cea93b6be04ec9a299
w3bdev1 <w3bdev@protonmail.com>
2021-12-07 21:34:21 +0530

    Seek setup

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const audioEl = document.getElementById('audio')
 const playBtn = document.getElementById('play')
+const seekSlider = document.getElementById('slider')
 
+// Seek Setup
+seekSlider.value = 0
+
+audioEl.addEventListener('loadedmetadata', () => {
+	seekSlider.max = audioEl.duration
+})
+
+audioEl.addEventListener('timeupdate', () => {
+	seekSlider.value = audioEl.currentTime
+})
+
+seekSlider.addEventListener('change', () => {
+	audioEl.currentTime = +seekSlider.value
+	if (audioEl.paused) {
+		audioEl.play()
+	}	
+})
+
+// Play/Pause
 playBtn.addEventListener('click', () => {
 	if (audioEl.paused) {
 		audioEl.play()
@@ -8,3 +28,4 @@ playBtn.addEventListener('click', () => {
 		audioEl.pause()
 	}
 })
+
